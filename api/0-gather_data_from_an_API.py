2c23b941c7975f6b4d03bd49fee948ac8a2e4c2a
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-15 11:02:26 +0200

    Python script that, using this REST API, for a given employee ID

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    root = "https://jsonplaceholder.typicode.com"
+    users = requests.get(root + "/users", params={"id": sys.argv[1]})
+    for names in users.json():
+        usr_id = names.get('id')
+        todo = requests.get(root + "/todos", params={"userId": usr_id})
+        task_complete = 0
+        tasks_array = []
+        for tasks in todo.json():
+            if tasks.get('completed') is True:
+                task_complete += 1
+                tasks_array.append(tasks.get('title'))
+        print("Employee {:s} is done with tasks({:d}/{:d}):\n\t {}".
+              format(names.get('name'), task_complete,
+                     len(todo.json()), "\n\t ".join(tasks_array)))
